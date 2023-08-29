6b23b185202db406f33b0c97704f472ff25753df
Shooterondeck77 <timothytaffur@gmail.com>
2023-08-28 20:49:59 -0400

    changes

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var myBtn = document.getElementById
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    window.location.href = "https://jesk3r.github.io/Cocktail-and-Courage";
+
+  });
