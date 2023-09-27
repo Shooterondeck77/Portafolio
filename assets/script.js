ed5f0cd17da5e326a015302c4d61adbe851cc7a6
Shooterondeck77 <timothytaffur@gmail.com>
2023-09-26 22:26:39 -0400

    added new project

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 var myBtn = document.getElementById
+var myBtn2 = document.getElementById
 
 document.getElementById("myBtn").addEventListener("click", function() {
     window.location.href = "https://jesk3r.github.io/Cocktail-and-Courage";
 
   });
+
+
+document.getElementById("myBtn2").addEventListener("click", function() {
+  window.location.href = "https://github.com/Shooterondeck77/sportstash"
+
+});
